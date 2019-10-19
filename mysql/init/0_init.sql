d92f46ae7944bb983475b5d557500104751052fb
Shogo Yasuda <shgysd@gmail.com>
2019-10-20 01:11:57 +0900

    update

--- mysql/init/0_init.sql
+++ mysql/init/0_init.sql
@@ -1,10 +1,14 @@
 CREATE DATABASE
-IF NOT EXISTS test_db;
+IF NOT EXISTS hash;
 
 CREATE TABLE
-IF NOT EXISTS test_db.test_table
+IF NOT EXISTS hash.users
 (
   id int,
-  name varchar
-(64)
+  name varchar(64),
+  display_name varchar(64),
+  email varchar(64),
+  password varchar(64),
+  created_at datetime  default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp
 );
--- src/go.mod
+++
@@ -1,5 +0,0 @@
-module rerem-api
-
-go 1.13
-
-require github.com/go-sql-driver/mysql v1.4.1
