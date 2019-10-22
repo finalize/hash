1b47d78da429bd2fba1bf638e689a1453b75faeb
Shogo Yasuda <shgysd@gmail.com>
2019-10-23 02:22:04 +0900

    use mysql for main db

--- api/main.go
+++ api/main.go
@@ -1,53 +1,28 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 
+	"github.com/shgysd/hash/api/config"
+	"github.com/shgysd/hash/api/db"
 	"github.com/shgysd/hash/api/handler"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+	config := config.New()
+
+	db.New(config)
+}
+
 func main() {
-	connectionString := getConnectionString()
-	db, err := sql.Open("mysql", connectionString)
-	if err != nil {
-		panic(err.Error())
-	}
+	db := db.GetDB()
 	defer db.Close()
 
 	mux := http.NewServeMux()
 	handler.InitializeRouter(db, mux)
 	fmt.Println("server is listening on port 8080")
 	http.ListenAndServe(":8080", mux)
-}
-
-func getConnectionString() string {
-	host := getParamString("MYSQL_DB_HOST", "localhost")
-	port := getParamString("MYSQL_PORT", "3306")
-	user := getParamString("MYSQL_USER", "root")
-	pass := getParamString("MYSQL_PASSWORD", "mysql")
-	dbname := getParamString("MYSQL_DB", "test_db")
-	protocol := getParamString("MYSQL_PROTOCOL", "tcp")
-	dbargs := getParamString("MYSQL_DBARGS", " ")
-
-	if strings.Trim(dbargs, " ") != "" {
-		dbargs = "?" + dbargs
-	} else {
-		dbargs = ""
-	}
-	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s",
-		user, pass, protocol, host, port, dbname, dbargs)
-}
-
-func getParamString(param string, defaultValue string) string {
-	env := os.Getenv(param)
-	if env != "" {
-		return env
-	}
-	return defaultValue
 }
