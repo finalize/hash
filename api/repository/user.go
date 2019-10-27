d57e1ffe4391b3d6287ae235aea7b831a36ef180
Shogo Yasuda <shgysd@gmail.com>
2019-10-27 20:29:26 +0900

    update

--- api/repository/user.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "github.com/shgysd/hash/api/types"
-
-// UserRepository Define user method
-type UserRepository interface {
-	SignUp(j *types.SignUp) int64
-}
--- api/types/types.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type SignUp struct {
-	Name        string `json:"name" validate:"required"`        //必須パラメータ
-	DisplayName string `json:"displayName" validate:"required"` //必須パラメータ
-	Email       string `json:"email" validate:"required,email"` //必須パラメータ、かつ、emailフォーマット
-	Password    string `json:"password" validate:"required"`
-}
